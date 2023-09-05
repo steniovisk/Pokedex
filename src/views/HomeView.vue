b9c996446ba77c5665ca60a494b60a38a0165efb
Stenio <stenioignacio@gmail.com>
2023-09-05 20:41:03 -0300

    Alteracao no nome 05-09-2023

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
 import { onMounted, reactive, ref } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue';
 
+let baseUrlSvg = ref("https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/other/dream-world/")
 let pokemons = reactive(ref());
 
 onMounted(() => {
@@ -35,7 +36,7 @@ onMounted(() => {
                         <div class="card-body row">
                             <ListPokemons
                             v-for="pokemon in pokemons"
-                            :key="pokemon.name"
+                            :key="pokemon.nome"
                             :name="pokemon.name"
                             :url ="pokemon.url"
                             />
